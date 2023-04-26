f8cbcb058e1fcf2d915cdad1cfe3fa4e68d6ee03
UrFriendKen <51278666+UrFriendKen@users.noreply.github.com>
2023-04-26 19:52:02 +0800

    init

---
+++ ChefOutfit/ColorableChefOutfit.cs
@@ -0,0 +1,123 @@
+﻿using KitchenData;
+using KitchenLib.References;
+using KitchenLib.Utils;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KitchenColorableCosmetics.ChefOutfit
+{
+    // Chef Outfit Prefab
+    // -- Armature
+    // -- Body
+    // -- Chef
+    // ------ Buttons
+    // ------ Hat
+
+    public abstract class ColorableChefOutfit : ColorablePlayerCosmetic
+    {
+        public override CosmeticType CosmeticType => CosmeticType.Outfit;
+        public override bool DisableInGame => false;
+        public override bool BlockHats => false;
+        public override List<RestaurantSetting> CustomerSettings => new List<RestaurantSetting>();
+        public override GameObject PrefabToCopy => (GDOUtils.GetExistingGDO(PlayerCosmeticReferences.ChefOutfit) as PlayerCosmetic).Visual;
+    }
+
+    public class BlueChefOutfit : ColorableChefOutfit
+    {
+        public override string UniqueNameID => "chefoutfitblue";
+
+        protected override List<BaseMaterialInstruction> MaterialsToApply => new List<BaseMaterialInstruction>()
+        {
+            new MaterialInstruction<SkinnedMeshRenderer>()
+            {
+                Path = "Chef/Chef",
+                Materials = new List<(string, bool)>()
+                {
+                    ("Clothing Blue", false)
+                }
+            },
+            new MaterialInstruction<SkinnedMeshRenderer>()
+            {
+                Path = "Chef/Chef/Buttons",
+                Materials = new List<(string, bool)>()
+                {
+                    ("Clothing Black Shiny", false)
+                }
+            },
+            new MaterialInstruction <SkinnedMeshRenderer>()
+            {
+                Path = "Chef/Chef/Hat",
+                Materials = new List<(string, bool)>()
+                {
+                    ("Clothing Blue", false)
+                }
+            }
+        };
+    }
+
+    public class GreenChefOutfit : ColorableChefOutfit
+    {
+        public override string UniqueNameID => "chefoutfitgreen";
+
+        protected override List<BaseMaterialInstruction> MaterialsToApply => new List<BaseMaterialInstruction>()
+        {
+            new MaterialInstruction<SkinnedMeshRenderer>()
+            {
+                Path = "Chef/Chef",
+                Materials = new List<(string, bool)>()
+                {
+                    ("Clothing Green", false)
+                }
+            },
+            new MaterialInstruction<SkinnedMeshRenderer>()
+            {
+                Path = "Chef/Chef/Buttons",
+                Materials = new List<(string, bool)>()
+                {
+                    ("Clothing Black Shiny", false)
+                }
+            },
+            new MaterialInstruction <SkinnedMeshRenderer>()
+            {
+                Path = "Chef/Chef/Hat",
+                Materials = new List<(string, bool)>()
+                {
+                    ("Clothing Green", false)
+                }
+            }
+        };
+    }
+
+    public class YellowChefOutfit : ColorableChefOutfit
+    {
+        public override string UniqueNameID => "chefoutfityellow";
+
+        protected override List<BaseMaterialInstruction> MaterialsToApply => new List<BaseMaterialInstruction>()
+        {
+            new MaterialInstruction<SkinnedMeshRenderer>()
+            {
+                Path = "Chef/Chef",
+                Materials = new List<(string, bool)>()
+                {
+                    ("Clothing - Yellow", false)
+                }
+            },
+            new MaterialInstruction<SkinnedMeshRenderer>()
+            {
+                Path = "Chef/Chef/Buttons",
+                Materials = new List<(string, bool)>()
+                {
+                    ("Clothing Black Shiny", false)
+                }
+            },
+            new MaterialInstruction <SkinnedMeshRenderer>()
+            {
+                Path = "Chef/Chef/Hat",
+                Materials = new List<(string, bool)>()
+                {
+                    ("Clothing - Yellow", false)
+                }
+            }
+        };
+    }
+}
---
+++ ColorablePlayerCosmetic.cs
@@ -0,0 +1,72 @@
+﻿using KitchenData;
+using KitchenLib.Customs;
+using KitchenLib.Utils;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KitchenColorableCosmetics
+{
+    public abstract class ColorablePlayerCosmetic : CustomPlayerCosmetic
+    {
+        protected abstract class BaseMaterialInstruction
+        {
+            public string Path;
+
+            // MaterialName, IsCustomMaterial
+            public List<(string, bool)> Materials;
+
+            protected Material[] _materialArray
+            {
+                get
+                {
+                    List<Material> tempList = new List<Material>();
+                    foreach ((string materialName, bool isCustomMaterial) in Materials)
+                    {
+                        Material material;
+                        switch (isCustomMaterial)
+                        {
+                            case true:
+                                material = MaterialUtils.GetCustomMaterial(materialName);
+                                break;
+                            default:
+                                material = MaterialUtils.GetExistingMaterial(materialName);
+                                break;
+                        }
+                        tempList.Add(material);
+                    }
+                    return tempList.ToArray();
+                }
+            }
+            public abstract void ApplyMaterials(GameObject gameObject);
+        }
+
+        protected class MaterialInstruction<T> : BaseMaterialInstruction where T : Renderer
+        {
+            public override void ApplyMaterials(GameObject gameObject)
+            {
+                gameObject.GetChild(Path).ApplyMaterial<T>(_materialArray);
+            }
+        }
+
+        public abstract GameObject PrefabToCopy { get; }
+
+        protected abstract List<BaseMaterialInstruction> MaterialsToApply { get; }
+
+        public override void OnRegister(PlayerCosmetic gameDataObject)
+        {
+            GameObject prefabHider = GameObject.Find("PrefabHider");
+            if (prefabHider == null)
+            {
+                prefabHider = new GameObject("PrefabHider");
+                prefabHider.SetActive(false);
+            }
+            gameDataObject.Visual = GameObject.Instantiate(PrefabToCopy);
+            gameDataObject.Visual.transform.SetParent(prefabHider.transform);
+
+            foreach (BaseMaterialInstruction instruction in MaterialsToApply)
+            {
+                instruction.ApplyMaterials(gameDataObject.Visual);
+            }
+        }
+    }
+}
